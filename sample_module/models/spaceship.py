84e27694f88ccae457f858cf55391c938758def4
sahidul-tw <121131705+sahidul-tw@users.noreply.github.com>
2024-08-21 13:17:33 -0400

    Update spaceship.py

--- sample_module/models/spaceship.py
+++ sample_module/models/spaceship.py
@@ -9,3 +9,13 @@ class Spaceship(models.Model):
     name = fields.Char(string="name")
     manufacture_date = fields.Date(string="Date")
     number = fields.Integer(string="Number")
+
+    cost_per_gallon = fields.Float()
+    gallons_per_tank = fields.Integer()
+
+    cost_per_tank = fields.Float(string="Cost to Fill Tank", compute='_compute_cost_per_tank')
+
+    @api.depends('cost_per_gallon', 'gallons_per_tank')
+    def _compute_cost_per_tank(self):
+        for spaceship in self:
+            spaceship.cost_per_tank = spaceship.cost_per_gallon * spaceship.gallons_per_tank
